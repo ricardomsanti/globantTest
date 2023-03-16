6588e87635665d20b2269c4a98d3aa3680d35fdd
ricardomsanti <ricardomsanti@google.com>
2023-03-16 03:24:43 -0300

    raw_files update with matchFiles method

--- code/apicode/raw_files.py
+++ code/apicode/raw_files.py
@@ -28,6 +28,28 @@ class RawFiles:
 
     #So far we gathered all the info about the reference files and the ones in the origin directory so we could
     # compare them and perform some validation in terms of the file's names, and the number of columns  
-        
+    def matchFiles(self):
+        matchList = []
+        #iterating - by name - untill finding dataframes that can be compared
+        for r in self.ref_list:
+            r_name = r["name"]
+            for o in self.origin_list:
+                o_name = o["name"]
+                #dataframe name match
+                if (r_name in o_name):
+                    #number of columns match
+                    r_cols =[c["name"] for c in r["cols"]]
+                    o_cols =[x for x in o["df"].columns]
+                    matchList.append([r_name, o_name, len(r_cols), len(o_cols)])                                      
+                    break
+                else:
+                    continue
+        matchListDf = pd.DataFrame(matchList, columns=["REFERENCE_NAME", "ORIGIN_NAME", "NO_COLS_REF", "NO_COLS_ORIGIN"])
+        return matchListDf
+
+
     
 rf = RawFiles()
+rf.originFiles()
+m = rf.matchFiles()
+print(m)
