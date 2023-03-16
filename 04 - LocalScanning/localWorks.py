0e534b016b8e6d0b9545d66ae56b74c35f65cbb6
ricardomsanti <ricardomsanti@google.com>
2023-03-15 23:13:34 -0300

    Added the first functional connection linking the api and the final powerbi report

--- 04 - LocalScanning/localWorks.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class LocalWorks():
-    def __init__(self, path=""):
-        self.path = path
-lw =    self.origin = D:\GIT_CODE\GLOBANT\globantTest\01 - basic_files 
-print()    
--- 05 - MainPaths/mp.py
+++
@@ -1,6 +0,0 @@
-
-class MainPaths():
-    def __init__(self):
-        self.local_oririn = "D://GIT_CODE//GLOBANT//globantTest//01 - basic_files"
-
-        
